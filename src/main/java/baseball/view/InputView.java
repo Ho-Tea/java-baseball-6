ca8ac1690591c70d660cc5c9d698f8f09ce792e2
yoonjuho <dbswn990@naver.com>
2023-12-04 21:27:22 +0900

    feat(InputView): 게임이 끝난 경우 재시작/종료를 구분하는 숫자를 입력받는다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -22,6 +22,15 @@ public class InputView {
                 .collect(Collectors.toUnmodifiableList());
     }
 
+    public int inputDecideNumber() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String decisionNumber = Console.readLine();
+        validateNullAndEmpty(decisionNumber);
+        validateNumeric(decisionNumber);
+        validateSingleLetter(decisionNumber);
+        return Integer.parseInt(decisionNumber);
+    }
+
     private void validateNullAndEmpty(String input) {
         if (Objects.isNull(input) || input.isEmpty()) {
             throw new IllegalArgumentException("null 이거나 길이가 없는 문자열 입니다.");
@@ -31,6 +40,11 @@ public class InputView {
     private void validateNumeric(String input) {
         if (!NUMERIC_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException("문자열이 숫자 1부터 9까지로 이루어져 있지 않습니다.");
+        }
+    }
+    private void validateSingleLetter(String input) {
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("문자열의 크기는 한개로 이루어져야 합니다.");
         }
     }
 }
