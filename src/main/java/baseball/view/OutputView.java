d994b11c046bb1330d70ead4d187b4a80e74175f
yoonjuho <dbswn990@naver.com>
2023-12-04 20:50:29 +0900

    feat(OutputView): 입력한 수에 대한 결과를 출력한다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,28 @@
 package baseball.view;
 
+import baseball.domain.GameHint;
+
+import java.util.Map;
+
 public class OutputView {
     public void printBegin(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printStatus(Map<GameHint, Integer> gameStatus){
+        if(gameStatus.containsKey(GameHint.NOTHING)){
+            System.out.println(GameHint.NOTHING.getKorean());
+            return;
+        }
+        String convertedStatus = attach(GameHint.BALL, gameStatus.get(GameHint.BALL))
+                + attach(GameHint.STRIKE, gameStatus.get(GameHint.STRIKE));
+        System.out.println(convertedStatus.trim());
+    }
+
+    private String attach(GameHint gameHint, int count) {
+        if (count != 0) {
+            return count + gameHint.getKorean() + " ";
+        }
+        return " ";
     }
 }
