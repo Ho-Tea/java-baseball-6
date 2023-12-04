42db83ae276ff080e744b35ed04164d6d55216f8
yoonjuho <dbswn990@naver.com>
2023-12-04 21:33:18 +0900

    feat(Game): 숫자 야구 게임 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.Game;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Game(new InputView(), new OutputView()).start();
     }
 }
---
+++ src/main/java/baseball/controller/Game.java
@@ -0,0 +1,35 @@
+package baseball.controller;
+
+import baseball.domain.*;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.Map;
+
+public class Game {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Game(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        outputView.printBegin();
+        do {
+            play(new Computer(new BaseballNumber(BaseballNumberGenerator.generate())));
+            outputView.printEnd();
+        } while (GameDecision.match(inputView.inputDecideNumber()).equals(GameDecision.RESTART));
+
+    }
+
+    private void play(Computer computer){
+        Map<GameHint, Integer> gameStatus = computer.compare(new BaseballNumber(inputView.inputNumbers()));
+        outputView.printStatus(gameStatus);
+        if(!computer.lose(gameStatus)){
+            play(computer);
+        }
+    }
+
+}
